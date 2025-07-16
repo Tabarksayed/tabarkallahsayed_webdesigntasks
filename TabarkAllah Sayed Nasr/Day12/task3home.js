608cac63ff8b5227aacc5a6bb59f5d86237d82a8
Tabark allah Sayed 20221040 <GitHubemail>
2025-07-16 08:48:30 +0300

    edit in day 12 tasks

--- TabarkAllah Sayed Nasr/Day12/task3home.js
+++
@@ -1,8 +0,0 @@
-
-let numCheck = parseInt(prompt("Enter a number to check if it's positive or negative:"));
-
-if (numCheck > 0) console.log("Positive");
-else if (numCheck < 0) console.log("Negative");
-else console.log("Zero");
-
-
--- TabarkAllah Sayed Nasr/Day12/task4home.js
+++
@@ -1,6 +0,0 @@
-
-let evenOdd = parseInt(prompt("Enter a number to check even or odd:"));
-if (evenOdd % 2 === 0) console.log("Even");
-else console.log("Odd");
-
-
